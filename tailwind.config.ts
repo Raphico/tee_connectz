de1923f1afdec0340d7d4d5ed1c66b23d181facf
Raphael Awoyinfa <raphicogit@gmail.com>
2023-10-22 15:28:10 -0700

    added default styles

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,21 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss"
 
 const config: Config = {
-  content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+   content: [
+      "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+      "./components/**/*.{js,ts,jsx,tsx,mdx}",
+      "./app/**/*.{js,ts,jsx,tsx,mdx}",
+   ],
+   theme: {
+      extend: {
+         colors: {
+            background: "hsl(var(--background))",
+            foreground: "hsl(var(--foreground))",
+            accent: "hsl(var(--accent))",
+            border: "hsl(var(--border))",
+         },
       },
-    },
-  },
-  plugins: [],
+   },
+   plugins: [],
 }
 export default config
