69db179396f09687dcc0f82a9b79ade3d148d5af
Raphael Awoyinfa <raphicogit@gmail.com>
2023-10-23 08:12:56 -0700

    adjusted blog page layout

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,13 +1,13 @@
 const BlogPage = () => {
    return (
-      <div>
-         <div className="text-center space-y-2 max-w-2xl mx-auto">
+      <div className="max-width">
+         <section className="max-w-2xl mx-auto text-center space-y-3">
             <h2 className="heading2">The Digital Marketer&apos;s Handbook</h2>
             <p className="body-text text-mute">
                Your go-to resource for digital marketing, social media
                management, brand promotion, and content creation
             </p>
-         </div>
+         </section>
       </div>
    )
 }
