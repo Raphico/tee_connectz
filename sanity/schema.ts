c98217a114e5065e423c91e24b6ce282c588b346
Raphael Awoyinfa <raphicogit@gmail.com>
2023-10-23 14:17:13 -0700

    removed author import

--- sanity/schema.ts
+++ sanity/schema.ts
@@ -1,6 +1,5 @@
 import { type SchemaTypeDefinition } from "sanity"
 import content from "./schemas/content"
-import author from "./schemas/author"
 import category from "./schemas/category"
 import post from "./schemas/post"
 
