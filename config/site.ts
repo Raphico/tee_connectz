37e97a30e9ee7df413db19ff0a99c831fbe92c4d
Raphael Awoyinfa <raphicogit@gmail.com>
2023-10-23 14:21:37 -0700

    modified git configuration

--- config/site.ts
+++ config/site.ts
@@ -2,8 +2,8 @@ export const siteConfig = {
    title: "Tee Connectz",
    description:
       "Tee connectz is a business that helps you to promote your brand, create engaging content, and manage your social media accounts. Whether you need to grow your brand awareness, reach new customers, or increase your sales, Tee connectz can help you achieve your goals with their digital marketing and social media expertise",
-   url: "https://tee_connectz.vercel.app",
-   og: "https://tee_connectz.vercel.app/og.png",
+   url: "https://tee-connectz.vercel.app",
+   og: "https://tee-connectz.vercel.app/og.png",
    links: [
       {
          name: "instagram",
