2b6e44966e8c810d0858a1056d574bffa2d93a30
Raphael Awoyinfa <raphicogit@gmail.com>
2023-10-28 07:42:59 -0700

    added sanity image remote config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
    experimental: { urlImports: ["https://themer.sanity.build/"] },
+   images: {
+      remotePatterns: [
+         {
+            protocol: "https",
+            hostname: "cdn.sanity.io",
+            port: "",
+         },
+      ],
+   },
 }
 
 module.exports = nextConfig
